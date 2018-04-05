80e48a448dad375bf4be6ff9ff05b98cdbc6d2e9
repjackson <repjackson@gmail.com>
2018-04-05 21:55:43 +0000

    working react hi world

--- client/main.js
+++ client/main.js
@@ -1,11 +1,11 @@
 // import '../imports/startup/client/index.js';
 
-import React from 'react'
+import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
 import App from '../imports/ui/App.js';
 
 Meteor.startup(() => {
-    render(<App />, document.getElementById('render-target'));
+  render(<App />, document.getElementById('render-target'));
 });
--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-
+import {withTracker} from 'meteor/react-meteor-data';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            test_session: true
-        };
-    }
-    
-    render() {
-        return (
-            <div className="container">
-                <h1>hello world</h1>
-               
-            </div>    
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      test_session: true
+    };
+  }
+  
+  render() {
+    return (
+      <div className="container">
+        <h1>hello world</h1>
+         
+      </div>    
+    )
+  }
 }
-    
+  
+export default withTracker(() => {
+
+  return {
+    currentUser: Meteor.user(),
+  };
+})(App);
