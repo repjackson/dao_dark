2e40150afec5c058d424ce61a597a01c694cc965
repjackson <repjackson@gmail.com>
2018-04-11 20:22:55 +0000

    working vuetify example, fuck yea

--- client/main.js
+++ client/main.js
@@ -1,7 +1,16 @@
 import {Meteor} from 'meteor/meteor';
 import {Vue} from 'meteor/akryum:vue';
+import Vuetify from 'vuetify';
+
+import 'vuetify/dist/vuetify.min.css'
+import 'vuetify/dist/vuetify.min.js' 
 
 import App from '/imports/ui/App.vue';
+
+Vue.use(Vuetify);
+// console.log(Vue)
+// console.log(Vuetify)
+
 
 Meteor.startup(() => {
   new Vue(App).$mount(document.body);
