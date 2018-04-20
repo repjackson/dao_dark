0273360b6468c14798484680f53e7f2c6330fcc1
repjackson <repjackson@gmail.com>
2018-04-20 05:29:43 +0000

    remove meteor candy

--- packages/meteor-candy-config/imports/server/_helpers/README.md
+++
@@ -1,3 +0,0 @@
-# Helpers
-
-These are some helper functions. Their main purpose is to cap some of the code to keep the example leans.
--- packages/meteor-candy-config/imports/server/_helpers/index.js
+++
@@ -1,8 +0,0 @@
-import { helpers as data } from './data.js'
-import { helpers as account } from './account.js'
-
-helpers = {} 
-helpers.data = data
-helpers.account = account
-
-export { helpers }
